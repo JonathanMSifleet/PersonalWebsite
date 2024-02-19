1f26e21a7b02c0a561faa72a467e2e5eea21cffe
Jonathan Sifleet <jonathanmsifleet@gmail.com>
2024-02-19 08:50:22 +0000

    debt: Convert Preact to React

---
+++ Client/src/app.tsx
@@ -0,0 +1,25 @@
+import { detect } from 'detect-browser';
+import { useEffect } from 'react';
+
+export const App = () => {
+  useEffect(() => {
+    if (localStorage.getItem('HAS_SHOWN_BROWSER_WARNING')) return;
+
+    const browser = detect()!.name;
+    if (!(browser === 'chrome' || browser === 'edge-chromium'))
+      alert(
+        'This website may not work properly on your browser. Please use Chrome or Edge if possible.'
+      );
+
+    localStorage.setItem('HAS_SHOWN_BROWSER_WARNING', 'true');
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Under construction for now!
+      </h1>
+      <p className="text-[12rem]">🏗️</p>
+    </>
+  );
+};
--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -1,29 +1,6 @@
-import { render } from 'preact';
 import './index.css';
-import { useEffect } from 'preact/hooks';
-import { detect } from 'detect-browser';
+import { App } from './app';
+import { createRoot } from 'react-dom/client';
 
-const App = () => {
-  useEffect(() => {
-    if (localStorage.getItem('HAS_SHOWN_BROWSER_WARNING')) return;
-
-    const browser = detect()!.name;
-    if (!(browser === 'chrome' || browser === 'edge-chromium'))
-      alert(
-        'This website may not work properly on your browser. Please use Chrome or Edge if possible.'
-      );
-
-    localStorage.setItem('HAS_SHOWN_BROWSER_WARNING', 'true');
-  }, []);
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Under construction for now!
-      </h1>
-      <p className="text-[12rem]">🏗️</p>
-    </>
-  );
-};
-
-render(<App />, document.getElementById('app')!);
+const root = createRoot(document.getElementById('app')!);
+root.render(<App />);
